d7a98910ca3ce4c2ab7c97211d50a8ce20d7780e
Bruno - Zeroc00I <b.minossi@gmail.com>
2022-01-16 17:09:42 -0300

    Create tab_name_and_description.py

---
+++ tab_name_and_description.py
@@ -0,0 +1,27 @@
+##
+#
+# It is my very first plugin attempt =)
+# and doesn't do anything at all
+# by @zeroc00I
+##
+
+from burp import IBurpExtender
+from burp import ITab
+from javax.swing import JScrollPane;
+
+class BurpExtender(IBurpExtender, ITab):
+
+    def	registerExtenderCallbacks(self, callbacks):
+        self._callbacks = callbacks
+        self._helpers = callbacks.getHelpers()
+        self.scrollpane = JScrollPane()
+        callbacks.setExtensionName("Description Name")
+        callbacks.customizeUiComponent(self.getUiComponent())
+        callbacks.addSuiteTab(self)     
+        return
+
+    def getTabCaption(self):
+        return 'Plugin Tab Name'
+
+    def getUiComponent(self):
+        return self.scrollpane
